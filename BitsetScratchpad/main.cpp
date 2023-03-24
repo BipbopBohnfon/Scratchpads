69c4b72dccb052cf0b539d7f7d2a8b70adaca756
Christopher Gemperle <chris_hg1@hotmail.com>
2023-03-24 14:42:57 -0700

    Removed Odin Repositories Added BitsetScratchpad and played with C Macros

--- BitsetScratchpad/main.cpp
+++ BitsetScratchpad/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+#define CHAR_BIT 8
+#define BITMASK(b) (1 << ((b) % CHAR_BIT))
+#define BITSLOT(b) ((b) / CHAR_BIT)
+#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
+#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
+#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
+#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char bitarray[BITNSLOTS(32)];
+
+    for (int i = 0; i < 32; i++) {
+        BITSET(bitarray, i);
+    }
+
+    BITCLEAR(bitarray, 17);
+    BITCLEAR(bitarray, 12);
+
+    for (int i = 0; i < 32; i++) {
+        if (BITTEST(bitarray, i)) std::cout << "TRUE";
+        else std::cout << "FALSE";
+        std::cout << " ";
+    }
+
     return 0;
+}
+
+void printBits(unsigned uint32_t) {
+
 }
