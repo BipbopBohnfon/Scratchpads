b7ca3658b357a56f703bebdb4a63a18ddcbf7837
Christopher Gemperle <chris_hg1@hotmail.com>
2023-03-26 15:25:18 -0700

    JsonCPP library built and linked locally, but going to use DaveGamble's cJSON instead

--- JsonCPP/main.cpp
+++ JsonCPP/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include "json/json.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Json::Value root;
+    std::ifstream inputfile;
+
+    inputfile.open("file.json");
+
+    Json::CharReaderBuilder builder;
+    builder["collectComments"] = true;
+    JSONCPP_STRING errs;
+
+    if (!parseFromStream(builder, inputfile, &root, &errs)) {
+        std::cout << errs << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cout << root << std::endl;
+    return EXIT_SUCCESS;
 }
